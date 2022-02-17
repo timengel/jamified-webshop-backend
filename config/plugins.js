355c2f588c8c156d9a4c6bb71405eb3553530dea
Tim Engel <tim.engel.lp@gmail.com>
2022-02-17 15:07:19 +0100

    add Cloudinary media provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,23 +1,23 @@
 
 module.exports = ({ env }) => ({
-  // upload: {
-  //   provider: 'cloudinary',
-  //   providerOptions: {
-  //     cloud_name: env('CLOUDINARY_NAME'),
-  //     api_key: env('CLOUDINARY_KEY'),
-  //     api_secret: env('CLOUDINARY_SECRET'),
-  //   },
-  //   actionOptions: {
-  //     upload: {
-  //       folder: 'jamified-webshop',
-  //       use_filename: true,
-  //       unique_filename: false,
-  //       overwrite: true,
-  //       async: false,
-  //       tags: ['jamified-webshop'],
-  //       invalidate: true,
-  //     },
-  //     delete: {},
-  //   },
-  // },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+    actionOptions: {
+      upload: {
+        folder: 'jamified-webshop',
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true,
+        async: false,
+        tags: ['jamified-webshop'],
+        invalidate: true,
+      },
+      delete: {},
+    },
+  },
 });
